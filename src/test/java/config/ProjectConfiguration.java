00ea00afee3e1523846c93051cadd1e52f36b728
Mikhail Turilov <turilovm@gmail.com>
2023-03-05 19:00:38 +0300

    added files

---
+++ src/test/java/config/ProjectConfiguration.java
@@ -0,0 +1,22 @@
+package config;
+import com.codeborne.selenide.Configuration;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import tests.TestBase;
+
+public class ProjectConfiguration {
+    public static void projectConfig(ProjectConfig projectConfig) {
+        Configuration.browserSize = projectConfig.getBrowserSize();
+        Configuration.browser = projectConfig.getBrowser();
+        Configuration.browserVersion = projectConfig.getBrowserVersion();
+        Configuration.baseUrl = projectConfig.getBaseUrl();
+
+        if(projectConfig.getIsRemote()) {
+            TestBase.selenideUrl = projectConfig.getSelenideUrl();
+            Configuration.remote = TestBase.selenideUrl+"wd/hub";
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("enableVNC", true);
+            capabilities.setCapability("enableVideo", true);
+            Configuration.browserCapabilities = capabilities;
+        }
+    }
+}
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -2,43 +2,34 @@ package tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.ProjectConfig;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.PracticeFormPage;
-import support.Attach;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.closeWebDriver;
+import static config.ProjectConfiguration.projectConfig;
+import static support.Attach.*;
 
 public class TestBase {
+    public static String selenideUrl;
     PracticeFormPage practiceFormPage = new PracticeFormPage();
-
+    private static final ProjectConfig config = ConfigFactory.create(ProjectConfig.class, System.getProperties());
     @BeforeAll
     static void beforeAll() {
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
+        projectConfig(config);
         Configuration.holdBrowserOpen = false;
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-    }
-    @BeforeEach
-    void addListener() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-    }
     @AfterEach
-    void addAttachments() {
-        Attach.screenshotAs("Last screenshot");
-        Attach.pageSource();
-        Attach.browserConsoleLogs();
-        Attach.addVideo();
+    public void tearDown() {
+        screenshotAs("Last screenshot");
+        pageSource();
+        browserConsoleLogs();
+        addVideo();
+        closeWebDriver();
     }
 }
